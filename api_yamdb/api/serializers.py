47513c877c04bd26c0d47013a0e26c2d4787f99d
Jultokm <jultokm@gmail.com>
2023-04-24 19:52:20 +0500

    перед объединением

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -68,6 +68,7 @@ class GenreSerializer(serializers.ModelSerializer):
 class TitlesSerializerRetrieve(serializers.ModelSerializer):
     category = CategorySerializer(read_only=True)
     genre = GenreSerializer(read_only=True, many=True)
+    rating = serializers.FloatField(read_only=True)
 
     class Meta:
         model = Titles
