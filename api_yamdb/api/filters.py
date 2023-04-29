9eb854b40e414ac34b52c96c65558e58b2716e45
Konstantin Gainbikhner <119727744+kegone@users.noreply.github.com>
2023-04-29 15:32:38 +0500

    index on (no branch): 27ab772 Выполнены правки по пользователям после ревью

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -1,22 +1,12 @@
-from rest_framework.filters import BaseFilterBackend
+from django_filters import CharFilter, FilterSet
+
+from reviews.models import Title
 
 
-class TitleFilterBackend(BaseFilterBackend):
-    def filter_queryset(self, request, queryset, view):
-        genre = request.query_params.get('genre')
-        if genre:
-            return queryset.filter(genre__slug=genre)
+class TitleFilterBackend(FilterSet):
+    genre = CharFilter(field_name='genre__slug')
+    category = CharFilter(field_name='category__slug')
 
-        category = request.query_params.get('category')
-        if category:
-            return queryset.filter(category__slug=category)
-
-        year = request.query_params.get('year')
-        if year:
-            return queryset.filter(year=year)
-
-        name = request.query_params.get('name')
-        if name:
-            return queryset.filter(name=name)
-
-        return queryset
+    class Meta:
+        model = Title
+        fields = ['year', 'name']
---
+++ example.py
@@ -0,0 +1,3 @@
+entries_by_visitor = [0, 'het', 2]
+
+print(entries_by_visitor[1])
